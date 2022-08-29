50d36c6b28496e9e13fb7b52102a47e516b2ec8e
nahmad <navid@horizondigital.net.au>
2022-08-29 17:09:05 +0500

    LoginPage

--- Oranghrms/src/test/java/oranghrms/Login.java
+++ Oranghrms/src/test/java/oranghrms/Login.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,10 +12,10 @@ import junit.framework.Assert;
 
 public class Login {
 	WebDriver driver = null;
-	
+
 	
 	@BeforeTest
-	public void setup() {
+	public void setup() throws InterruptedException {
 		
 		//String Path = System.getProperty("user.dir");
 		//System.setProperty("webdriver.driver.chromedriver", Path+ "/folder//Oranghrms/Chrome/chromedriver");
@@ -22,61 +23,140 @@ public class Login {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(2000);
 
+		
 	}
 	@Test(priority=1)
-	public void matchtitle() throws InterruptedException
+	public void A_company_bImage() throws InterruptedException {
+		WebElement image = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[1]/img"));
+
+	if(image.isDisplayed()) {
+	System.out.println("Company-Branding Available");
+	}
+	else {
+	System.out.println("Company-Branding Missing");}
+	Thread.sleep(2000);}
+
+	//**************************Get Orangehrm-Logo at Login page******************************
+	@Test(priority=2)
+	public void B_OrangHRM_Logo() throws InterruptedException 
 	{
-		
-		String ActualTitle = driver.getTitle();
-		String ExpectedTitle="OrangeHRM";
-		Assert.assertEquals(ExpectedTitle, ActualTitle);
-		System.out.println("Actual Result ="+ActualTitle );
-		Thread.sleep(15000);
-		}
-		@Test(priority=2)
-		public void imagecapture() throws InterruptedException {
-			WebElement image = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[1]/img"));
-		
-		if(image.isDisplayed()) {
-				System.out.println("company-branding available");
-			}
-		else {
-			System.out.println("company-branding missing");
-			Thread.sleep(5000);
+		WebElement Oranghrlogo = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/img"));
+			//Applied the condition through if else statement 
+	if(Oranghrlogo.isDisplayed()) {
+	System.out.println("OrangeHRM-Logo is Available");
+				}
+	else {
+	System.out.println("OrangeHRM-Logo is Missing");}
+	Thread.sleep(2000);
+	
+	}
+	
+	// Get Page heading(Login)
+	@Test(priority=3)
+	public void Page_Heading() throws InterruptedException
+	{
+		String text = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/h5")).getText();
+	String ActualText = "Login";
+	String ExpectedText= text;
+	AssertJUnit.assertEquals(ExpectedText, ActualText);
+	//Applied the condition through if else statement
+	if(ExpectedText==text) {
+	System.out.println("Your Expected Heading is = "+text);}
+	else {
+	System.out.println("Your Expected Heading is incorrect");}
+	Thread.sleep(2000);}
+	// Get UserName_Label(Username)
+	@Test(priority=4)
+	public void Username() throws InterruptedException
+	{
+		String Username = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]"
+					+ "/form/div[1]/div/div[1]/label")).getText();
+	String ActualLabel = "Username";
+	String ExpectedLabel= Username;
+	AssertJUnit.assertEquals(ExpectedLabel, ActualLabel);
 			
+			//Applied the condition through if else statement
+	if(ExpectedLabel==Username) {
+	System.out.println("Your Expected Label is = "+Username);}
+	else {
+	System.out.println("Your Expected Label is incorrect");}
+				Thread.sleep(3000);}
 			
-		}}
-		
-		@Test(priority=3)
-		public void LoginText() throws InterruptedException {
-			String  text = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/h5")).getText();
-			Thread.sleep(3000);
-			String ActualText = "Login";
-			String ExpectedText= text;
-			Assert.assertEquals(ExpectedText, ActualText);
-			if(ExpectedText==text) {
-				System.out.println("Your Expected text is correct");
-
-			}
-			else {
-				System.out.println("Your Expected text is incorrect");
-
-			}
-		}
-		@Test(priority=4)
-		public void userpassword() throws InterruptedException {
+			// Get Password_Label(Password)
+	@Test(priority=5)
+	public void Password() throws InterruptedException
+	{
+	String password = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]"
+					+ "/form/div[2]/div/div[1]/label")).getText();
+	String ActualPassword = "Password";
+	String ExpectedPassword= password;
+	AssertJUnit.assertEquals(ExpectedPassword, ActualPassword);
+			
+			//Applied the condition through if else statement
+	if(ExpectedPassword==password) {
+	System.out.println("Your Expected Label is = "+password);}
+	else {
+	System.out.println("Your Expected Label is incorrect");}
+			Thread.sleep(3000);}
+			//Get LoginButton_Label(Login)
+	@Test(priority=6)
+	public void Login_Button() throws InterruptedException {
+		String Btnlogin = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/"
+					+ "div/div[2]/div[2]/form/div[3]/button")).getText();
+	String ActualBtnLabel = "Login";
+	String ExpectedBtnLabel= Btnlogin;
+	AssertJUnit.assertEquals(ExpectedBtnLabel, ActualBtnLabel);
+			
+			//Applied the condition through if else statement
+	if(ExpectedBtnLabel==Btnlogin) {
+	System.out.println("Your Expected Label is = "+Btnlogin);}
+	else {
+	System.out.println("Your Expected Label is incorrect");
+	Thread.sleep(3000);
+	}
+	;}
+//			//***********************Click at Linkedin link****************
+	@Test(priority=7)
+	public void Linkedin() throws InterruptedException
+	{
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/"
+				+ "div/div[2]/div[3]/div[1]/a[1]")).click();
+			Thread.sleep(2000);}
+		//***********************Click at Facebook link****************
+	@Test(priority=8)
+	public void Facebook() throws InterruptedException
+				{
+				driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/"
+				+ "div/div[2]/div[3]/div[1]/a[2]")).click(); 
+				Thread.sleep(2000);}
+		//***********************Click at Twitter link****************
+	@Test(priority=9)
+			public void Twitter()
+						{
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[3]/div[1]/a[4]")).click();
+						}
+			
+		//***********************Click at Youtube link****************
+	@Test(priority=10)
+			public void Youtube() throws InterruptedException{
+	driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[3]/div[1]/a[4]")).click();
+	Thread.sleep(2000);
+	}
+	@Test(priority=11)
+	public void login() throws InterruptedException {
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
-		}
-
+	}
 		
 	@AfterTest
 	public void teardown() 
 	{
 		driver.close();
+		driver.quit();
 		
 	}
 	
